96af08e69896df1fc79ca7329eb4768841a0d531
Emma Bastås <emma.bastas@protonmail.com>
2020-08-08 14:44:43 +0200

    Remove

--- src/String/Template.elm
+++ src/String/Template.elm
@@ -1,11 +1,8 @@
-module String.Template exposing (inject, injectSafe, Error)
+module String.Template exposing (inject)
 
-{-| There's two functions to inject values into a string. `inject` and `injectSafe`.
+{-|
 
-  - `inject` is meant for a static template and key-value pairs.
-  - `injectingSafe` can be used with dynamic data where some placeholders might miss values to inject. In that case an `Error` will be returned detailing the problem.
-
-@docs inject, injectSafe, Error
+@docs inject
 
 -}
 
@@ -42,68 +39,6 @@ inject substitutions template =
             )
 
 
-{-| Inject values into a string with the given key-value pairs. This function checks that each placeholder has been replaced with a value and is thus suitable for cases where the template and/or key-vaue pairs are dynamic.
-
-    "Good day ${title} ${lastName}!"
-        |> String.Template.injectSafe
-            [ ( "title", "dr.")
-            ] -- We forgot "lastName"
-
-    -- Err [ { placeholderName = "lastName", placeholderRange = (18, 29) } ]
-
--}
-injectSafe : List ( String, String ) -> String -> Result (List Error) String
-injectSafe substitutions template =
-    let
-        dict =
-            Dict.fromList substitutions
-    in
-    template
-        |> Regex.find regex
-        |> List.foldl
-            (\{ match, index } ( errors, s ) ->
-                let
-                    placeholderName =
-                        placeholderNameFromPlaceholder match
-
-                    startIndex =
-                        index
-
-                    endIndex =
-                        index + String.length match
-                in
-                case Dict.get placeholderName dict of
-                    Just substitution ->
-                        ( errors
-                        , replaceSlice substitution startIndex endIndex s
-                        )
-
-                    Nothing ->
-                        ( { placeholderName = placeholderName
-                          , placeholderRange = ( startIndex, endIndex )
-                          }
-                            :: errors
-                        , s
-                        )
-            )
-            ( [], template )
-        |> (\( errors, renderedTemplate ) ->
-                if List.length errors == 0 then
-                    Ok renderedTemplate
-
-                else
-                    Err errors
-           )
-
-
-{-| The error returned by `injectingSafe`.
--}
-type alias Error =
-    { placeholderName : String
-    , placeholderRange : ( Int, Int )
-    }
-
-
 regex : Regex
 regex =
     Regex.fromString "\\${[^}]*}"
@@ -122,12 +57,7 @@ placeholderNameFromPlaceholder placeholder =
 
 
 
--- replaceSlice, dropwWhile and dropWhileRight taken from elm-community/list-extra
-
-
-replaceSlice : String -> Int -> Int -> String -> String
-replaceSlice substitution start end string =
-    String.slice 0 start string ++ substitution ++ String.slice end (String.length string) string
+-- dropwWhile and dropWhileRight taken from elm-community/list-extra
 
 
 dropWhile : (a -> Bool) -> List a -> List a
